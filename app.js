ddbe3a45e97fdf98c26761ea41734b508c92925b
rdelva <roseldelva@gmail.com>
2021-11-26 20:05:16 -0500

    add in requires to import routes

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 //Import Express and set up the app
 const express = require('express');
 const app = express();
+const path = require('path');
+
+
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Import Routes
+const routes = require('./routes');
+app.use(routes);
+
 
 //Turn on Express  Server
 app.listen(3000, () =>{
     console.log('Server listening on port 3000');
-})
+});
+
