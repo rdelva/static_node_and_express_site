5ba0b1705ed3af56f796244c048348753d4606f3
rdelva <roseldelva@gmail.com>
2021-11-26 20:05:54 -0500

    add in index route

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { projects } = require('../data/data.json');
+
+
+router.get('/', function(req, res, next) {
+    res.render('index', { projects });
+      
+});
+
+module.exports = router;
