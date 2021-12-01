0246ab5f7a97da525c383ae393baa3a6929cf267
rdelva <roseldelva@gmail.com>
2021-11-30 21:56:16 -0500

    added in projects path

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,21 @@ router.get('/', function(req, res, next) {
 });
 
 
+//Project Pages
 
+router.get('/project/:id', (req, res, next) => {
+
+  const projectId = req.params.id;
+  const project =  projects.find( ({ id }) => id === + projectId );
+
+  if(project) {
+    // Pass the project data to the project template
+    res.render('project', { projects} );
+  } else {
+    res.sendStatus(404);
+  }
+
+});
 
 module.exports = router;
 
